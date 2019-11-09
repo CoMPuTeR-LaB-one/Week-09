732dfb236a8836984f33b52685250bf14f798b06
WidsawaD <safefire@gmail.com>
2019-11-09 09:28:58 +0700

    การวาดส่วนโค้ง (Arc)

--- 61030243/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030243/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace GDIPlus_1
 {
@@ -21,17 +22,14 @@ namespace GDIPlus_1
         {
             Graphics g = e.Graphics;
 
-            Rectangle rect = new Rectangle(10, 10, 100, 100);
-            g.DrawEllipse(Pens.Purple, rect);
+            Pen penEllipse = new Pen(Color.Brown);
+            penEllipse.DashStyle = DashStyle.Dash;
+            g.DrawEllipse(penEllipse, 20, 20, 200, 150);
 
-            Rectangle rect1 = new Rectangle(10, 120, 100, 100);
-            g.FillEllipse(Brushes.DeepPink, rect1);
+            Pen penArc = new Pen(Color.Magenta, 2);
+            g.DrawArc(penArc, 20, 20, 200, 150, 90, 180);
 
-            Rectangle rect2 = new Rectangle(150, 10, 120, 100);
-            g.DrawEllipse(Pens.DarkSlateBlue, rect2);
-
-            Rectangle rect3 = new Rectangle(150, 120, 120, 100);
-            g.FillEllipse(Brushes.Firebrick, rect3);
+            g.Dispose();
         }
     }
 }
