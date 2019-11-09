09c9f52a802966b4ddbb12733239d083cab62d7e
WidsawaD <safefire@gmail.com>
2019-11-09 10:33:00 +0700

    การระบายสีด้วย TextureBrush บน panel

--- 61030243/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030243/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -21,18 +21,21 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            try
-            {
-                Image image = Image.FromFile("D:\\saitama.JPG");
-                TextureBrush brush = new TextureBrush(image);
-                Rectangle rect = new Rectangle(10, 10, 180, 150);
-                g.FillRectangle(brush, rect);
-            }
-            catch (Exception a)
-            {
-                MessageBox.Show(a.ToString());
-            }
             g.Dispose();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            LinearGradientBrush pnlGdt = new LinearGradientBrush(panel1.ClientRectangle, Color.Yellow, Color.Navy, 0f, true);
+            e.Graphics.FillRectangle(pnlGdt, panel1.ClientRectangle);
+            pnlGdt.Dispose();
+        }
+
+        private void Panel2_Paint(object sender, PaintEventArgs e)
+        {
+            LinearGradientBrush pnlGdt = new LinearGradientBrush(panel2.ClientRectangle, Color.Yellow, Color.Navy, 90f, true);
+            e.Graphics.FillRectangle(pnlGdt, panel2.ClientRectangle);
+            pnlGdt.Dispose();
         }
     }
 }
